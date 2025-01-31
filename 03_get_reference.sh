a1d91119b0db1fdc76aae185328e30d73027f5b9
Ramakanth Kumble <rkumble@login8.hpc.binf.unibe.ch>
2025-01-31 14:32:01 +0100

    ref genome and index scripts

---
+++ 03_get_reference.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#SBATCH --time=00:10:00
+#SBATCH --mem=10M
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=get_reference
+#SBATCH --output=/data/users/rkumble/rnaseq_course/log/03_get_reference_%J.out
+#SBATCH --error=/data/users/rkumble/rnaseq_course/log/03_get_reference_%J.err
+#SBATCH --partition=pibu_el8
+
+# Variables
+WORKDIR="/data/users/rkumble/rnaseq_course/"
+REFGENDIR="$WORKDIR/reference_genome"
+LOGDIR="$WORKDIR/log"
+
+#Create the directory for the error and output file
+mkdir -p $LOGDIR
+mkdir -p $REFGENDIR
+
+#cd into folder with the reference genome and download fa and gff file from ensembl
+cd $REFGENDIR
+wget https://ftp.ensembl.org/pub/release-113/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.primary_assembly.fa.gz
+wget https://ftp.ensembl.org/pub/release-113/gtf/mus_musculus/Mus_musculus.GRCm39.113.gtf.gz
+
+#Verify the integrity of the files (to detect any corruption)
+echo "Checksum for fasta file"
+sum $REFGENDIR/Mus_musculus.GRCm39.dna.primary_assembly.fa.gz
+echo "Checksum for gtf file"
+sum $REFGENDIR/Mus_musculus.GRCm39.113.gtf.gz 
---
+++ 04_histat_index.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#SBATCH --time=03:00:00
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=hisat_index
+#SBATCH --output=/data/users/rkumble/rnaseq_course/log/04_hisat_index_%J.out
+#SBATCH --error=/data/users/rkumble/rnaseq_course/log/04_hisat_index_%J.err
+#SBATCH --partition=pibu_el8
+
+#Variables
+WORKDIR="/data/users/${USER}/rnaseq_course/"
+REFGENDIR="$WORKDIR/reference_genome"
+LOGDIR="$WORKDIR/log"
+INDEXDIR="$WORKDIR/index_hisat"
+REFGENOMEFILE="Mus_musculus.GRCm39.dna.primary_assembly.fa"
+
+
+#Create the directory for the error and output file
+mkdir -p $LOGDIR
+
+mkdir -p $INDEXDIR
+
+#unzip the file for indexing 
+gunzip $REFGENDIR/$REFGENOMEFILE.gz
+
+#Run hisat
+apptainer exec --bind $WORKDIR /containers/apptainer/hisat2_samtools_408dfd02f175cd88.sif hisat2-build $REFGENDIR/$REFGENOMEFILE $INDEXDIR/genome_index
